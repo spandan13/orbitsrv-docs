32b2cbc075d218b48b9cb6df77cbc2f586b1b62c
Spandan A <100998511+spandan13@users.noreply.github.com>
2022-08-23 17:23:58 +0530

    Update telegram-temp.py

--- telegram-temperature-notifier/telegram-temp.py
+++ telegram-temperature-notifier/telegram-temp.py
@@ -6,12 +6,15 @@ def rpi_temp():
     cpu_temp = os.popen("vcgencmd measure_temp").readline()
     return cpu_temp.replace("temp=", "")
 
+
 API_KEY = "" #Add telegram bot API Key here
 userid = "" # Add telegram userid here
-updater = Updater(API_KEY, use_context=True)
-   
+temp_limit = 70.0 # Change to your prefered limit
+
+
+updater = Updater(API_KEY, use_context=True)  
 temp = round((gz.CPUTemperature().temperature), 1)
-if float(temp) > 55.0:
+if float(temp) > temp_limit:
 
     message = "🔴 Current CPU Temp = " + str(temp) + "'C"
     updater.bot.send_message(userid, message)
