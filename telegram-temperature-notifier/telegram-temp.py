665be89b505dda9575521444f14037f5bb4aa90c
Spandan A <100998511+spandan13@users.noreply.github.com>
2022-08-23 17:15:29 +0530

    Create telegram-temp.py

---
+++ telegram-temperature-notifier/telegram-temp.py
@@ -0,0 +1,18 @@
+import time
+from telegram.ext import *
+import gpiozero as gz
+
+def rpi_temp():
+    cpu_temp = os.popen("vcgencmd measure_temp").readline()
+    return cpu_temp.replace("temp=", "")
+
+API_KEY = '5463095470:AAGS5HWBmhUGhl2KEtbFReZLqvNG6zcTTSs'
+updater = Updater(API_KEY, use_context=True)
+    
+#while True:
+    #temp = rpi_temp().replace("'C\n", "")
+temp = round((gz.CPUTemperature().temperature), 1)
+if float(temp) > 55.0:
+
+    message = "🔴 Current CPU Temp = " + str(temp) + "'C"
+    updater.bot.send_message("-619896582", message)
